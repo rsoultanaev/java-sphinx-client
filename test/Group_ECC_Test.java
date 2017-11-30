848daaa25d6bc5259957af1e22012c31db530f75
rsoultanaev <robert.soultanaev@hotmail.com>
2017-11-30 18:48:16 +0000

    Use Hex util from bouncycastle

--- test/Group_ECC_Test.java
+++ test/Group_ECC_Test.java
@@ -1,4 +1,5 @@
 import org.bouncycastle.math.ec.ECPoint;
+import org.bouncycastle.util.encoders.Hex;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,18 +13,6 @@ public class Group_ECC_Test {
 
     private Group_ECC group_ecc;
 
-    private String bytesToHex(byte[] bytes) {
-        char[] hexArray = "0123456789abcdef".toCharArray();
-
-        char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-
     @Before
     public void setUp() {
         group_ecc = new Group_ECC();
@@ -35,24 +24,24 @@ public class Group_ECC_Test {
 
         ECPoint base = group_ecc.getGenerator();
 
-        String expectedOutput1 = "02a66335a59f1277c193315eb2db69808e6eaf15c944286765c0adcae2";
-        String output1 = bytesToHex(group_ecc.expon(base, secret).getEncoded(true));
+        String expectedOutput = "02a66335a59f1277c193315eb2db69808e6eaf15c944286765c0adcae2";
+        String output = Hex.toHexString(group_ecc.expon(base, secret).getEncoded(true));
 
-        assertEquals(expectedOutput1, output1);
+        assertEquals(expectedOutput, output);
     }
 
     @Test
     public void multiexpon() throws Exception {
         BigInteger secret1 = new BigInteger("10242318609670578569309311701916918226942711495988531232197429015905");
         BigInteger secret2 = new BigInteger("9166896489953568699130350165214278503117209070949180823539577781184");
-        
+
         ECPoint base = group_ecc.getGenerator();
         List<BigInteger> exponents = Arrays.asList(secret1, secret2);
 
-        String expectedOutput1 = "03085f86c52bbb391e7fba0dd1e39541fe89ac5b6afd576c338948abe0";
-        String output1 = bytesToHex(group_ecc.multiexpon(base, exponents).getEncoded(true));
+        String expectedOutput = "03085f86c52bbb391e7fba0dd1e39541fe89ac5b6afd576c338948abe0";
+        String output = Hex.toHexString(group_ecc.multiexpon(base, exponents).getEncoded(true));
 
-        assertEquals(expectedOutput1, output1);
+        assertEquals(expectedOutput, output);
     }
 
 }
