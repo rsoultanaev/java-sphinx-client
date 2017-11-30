391a78faa9e92f771ce1dca664bdc5bdd47f36f7
rsoultanaev <robert.soultanaev@hotmail.com>
2017-11-30 18:33:09 +0000

    Remove unnecessary tests from Group_ECC_Test

--- test/Group_ECC_Test.java
+++ test/Group_ECC_Test.java
@@ -10,10 +10,6 @@ import static org.junit.Assert.*;
 
 public class Group_ECC_Test {
 
-    private BigInteger secret1;
-    private BigInteger secret2;
-    private BigInteger secret3;
-
     private Group_ECC group_ecc;
 
     private String bytesToHex(byte[] bytes) {
@@ -30,49 +26,33 @@ public class Group_ECC_Test {
 
     @Before
     public void setUp() {
-        secret1 = new BigInteger("10242318609670578569309311701916918226942711495988531232197429015905");
-        secret2 = new BigInteger("9166896489953568699130350165214278503117209070949180823539577781184");
-        secret3 = new BigInteger("7306929579332400726108483579260842691270825133579789121406740491622");
-
         group_ecc = new Group_ECC();
     }
 
     @Test
     public void expon() throws Exception {
-        String expectedOutput1 = "02a66335a59f1277c193315eb2db69808e6eaf15c944286765c0adcae2";
-        String expectedOutput2 = "0211a8818e3e4c93f5bdd1fb4961630309206e61a9653d4f5bd0821455";
-        String expectedOutput3 = "02686c1f9d7fce854cdf51046948bec184fed66dd49554f0ce856d6f21";
+        BigInteger secret = new BigInteger("10242318609670578569309311701916918226942711495988531232197429015905");
 
         ECPoint base = group_ecc.getGenerator();
 
-        String output1 = bytesToHex(group_ecc.expon(base, secret1).getEncoded(true));
-        String output2 = bytesToHex(group_ecc.expon(base, secret2).getEncoded(true));
-        String output3 = bytesToHex(group_ecc.expon(base, secret3).getEncoded(true));
+        String expectedOutput1 = "02a66335a59f1277c193315eb2db69808e6eaf15c944286765c0adcae2";
+        String output1 = bytesToHex(group_ecc.expon(base, secret).getEncoded(true));
 
         assertEquals(expectedOutput1, output1);
-        assertEquals(expectedOutput2, output2);
-        assertEquals(expectedOutput3, output3);
     }
 
     @Test
     public void multiexpon() throws Exception {
-        String expectedOutput1 = "03085f86c52bbb391e7fba0dd1e39541fe89ac5b6afd576c338948abe0";
-        String expectedOutput2 = "02bb81557527fe5b00c896006836276f5de1041681c8714060e25995fe";
-        String expectedOutput3 = "03bba85f69eb9ab0a8a0b9b4b0e9c84aba412b36083d8c6f10c13a58ce";
-
-        List<BigInteger> exponents1 = Arrays.asList(secret1, secret2);
-        List<BigInteger> exponents2 = Arrays.asList(secret1, secret3);
-        List<BigInteger> exponents3 = Arrays.asList(secret2, secret3);
-
+        BigInteger secret1 = new BigInteger("10242318609670578569309311701916918226942711495988531232197429015905");
+        BigInteger secret2 = new BigInteger("9166896489953568699130350165214278503117209070949180823539577781184");
+        
         ECPoint base = group_ecc.getGenerator();
+        List<BigInteger> exponents = Arrays.asList(secret1, secret2);
 
-        String output1 = bytesToHex(group_ecc.multiexpon(base, exponents1).getEncoded(true));
-        String output2 = bytesToHex(group_ecc.multiexpon(base, exponents2).getEncoded(true));
-        String output3 = bytesToHex(group_ecc.multiexpon(base, exponents3).getEncoded(true));
+        String expectedOutput1 = "03085f86c52bbb391e7fba0dd1e39541fe89ac5b6afd576c338948abe0";
+        String output1 = bytesToHex(group_ecc.multiexpon(base, exponents).getEncoded(true));
 
         assertEquals(expectedOutput1, output1);
-        assertEquals(expectedOutput2, output2);
-        assertEquals(expectedOutput3, output3);
     }
 
 }
