e959d16c52d7abfd6b21f8407ac44a53f06ca61b
rsoultanaev <robert.soultanaev@hotmail.com>
2017-11-30 19:49:39 +0000

    Add SphinxParams stubs

--- src/SphinxParams.java
+++ src/SphinxParams.java
@@ -1,33 +1,63 @@
-import org.bouncycastle.crypto.CipherParameters;
-import org.bouncycastle.crypto.digests.SHA256Digest;
-import org.bouncycastle.crypto.engines.AESEngine;
-import org.bouncycastle.crypto.modes.SICBlockCipher;
-import org.bouncycastle.crypto.params.KeyParameter;
-import org.bouncycastle.crypto.params.ParametersWithIV;
-import org.bouncycastle.util.encoders.Hex;
 
 public class SphinxParams {
 
-    public String aesCtr(String message, String iv, String key) {
-        CipherParameters params = new ParametersWithIV(new KeyParameter(Hex.decode(key)), Hex.decode(iv));
-        SICBlockCipher engine = new SICBlockCipher(new AESEngine());
-
-        engine.init(true, params);
-
-        byte[] cipher = new byte[5 * 16];
-        byte[] plain = Hex.decode(message);
-
-        engine.processBytes(plain, 0, plain.length, cipher, 0);
-
-        return Hex.toHexString(cipher);
+    public byte[] aesCtr(byte[] key, byte[] message, byte[] iv) {
+        return null;
     }
 
-    public String hash(String input) {
-        SHA256Digest digest = new SHA256Digest();
-        byte[] inputBytes = Hex.decode(input);
-        byte[] output = new byte[digest.getDigestSize()];
-        digest.update(inputBytes, 0, inputBytes.length);
-        digest.doFinal(output, 0);
-        return Hex.toHexString(output);
+    public byte[] lionessEnc(byte[] key, byte[] message) {
+        return null;
+    }
+
+    public byte[] lionessDec(byte[] key, byte[] message) {
+        return null;
+    }
+
+    public byte[] xorRho(byte[] key, byte[] plain) {
+        return null;
+    }
+
+    public byte[] mu(byte[] key, byte[] data) {
+        return null;
+    }
+
+    public byte[] pi(byte[] key, byte[] data) {
+        return null;
+    }
+
+    public byte[] pii(byte[] key, byte[] data) {
+        return null;
+    }
+
+    public byte[] hash(byte[] data) {
+        return null;
+    }
+
+    public byte[] getAesKey(byte[] s) {
+        return null;
+    }
+
+    public byte[] deriveKey(byte[] k, byte[] flavor) {
+        return null;
+    }
+
+    public byte[] hb(byte[] alpha, byte[] k) {
+        return null;
+    }
+
+    public byte[] hrho(byte[] k) {
+        return null;
+    }
+
+    public byte[] hmu(byte[] k) {
+        return null;
+    }
+
+    public byte[] hpi(byte[] k) {
+        return null;
+    }
+
+    public byte[] htau(byte[] k) {
+        return null;
     }
 }
