609cd4b3e65c64811d901ccbd873ca75bceff44d
rsoultanaev <robert.soultanaev@hotmail.com>
2017-12-02 15:13:18 +0000

    Add test stubs for SphinxParams_Test

--- test/SphinxParams_Test.java
+++ test/SphinxParams_Test.java
@@ -12,4 +12,79 @@ public class SphinxParams_Test {
         params = new SphinxParams();
     }
 
+    @Test
+    public void aesCtrEncrypt() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void aesCtrEncryptThenDecrypt() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void lionessEncrypt() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void lionessEncryptThenDecrypt() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void xorRho() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void mu() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void piEncryptThenDecrypt() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void hash() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void getAesKey() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void deriveKey() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void hb() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void hrho() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void hmu() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void hpi() throws Exception {
+        assertTrue(false);
+    }
+
+    @Test
+    public void htau() throws Exception {
+        assertTrue(false);
+    }
+
 }
