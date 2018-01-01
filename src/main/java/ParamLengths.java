f61d500f2f103d9bb7f035a1e4b8fc6b0aaed25c
Robert Soultanaev <robert.soultanaev@hotmail.com>
2018-01-01 21:14:59 +0200

    Remove equals and hashcode from ParamLengths

--- src/main/java/ParamLengths.java
+++ src/main/java/ParamLengths.java
@@ -6,22 +6,4 @@ public class ParamLengths {
         this.headerLength = headerLength;
         this.bodyLength = bodyLength;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ParamLengths paramLengths = (ParamLengths) o;
-
-        if (headerLength != paramLengths.headerLength) return false;
-        return bodyLength == paramLengths.bodyLength;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = headerLength;
-        result = 31 * result + bodyLength;
-        return result;
-    }
 }
